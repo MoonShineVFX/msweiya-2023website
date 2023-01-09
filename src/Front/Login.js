fce95f2f96164019054b4e46ac4f8b198c0e0439
wood.wu <kilokingw@gmail.com>
2023-01-09 20:22:42 +0800

    Updated login page

--- src/Front/Login.js
+++ src/Front/Login.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import {auth} from '../firebaseConfig/fireauth'
 import {  signInWithEmailAndPassword , onAuthStateChanged } from "firebase/auth";
 import { AuthContext } from "../Auth";
-
+import { motion } from "framer-motion"
 function Login() {
   const [user , setUser] = useState({})
   onAuthStateChanged(auth , (currentUser)=>{
@@ -30,9 +30,48 @@ function Login() {
     return <Navigate to="/admin" />;
   }
   return (
-    <div className="login  ">
-      Login
-      
+    <div 
+      className="login  "
+    >
+      <div
+        className="h-[80vh] bg-no-repeat bg-cover bg-center absolute top-0 w-full -z-20"
+        style={{backgroundImage : `url(${process.env.PUBLIC_URL+'/images/bg.png'})`}}></div>
+      <div 
+        className=' absolute h-[80vh] top-0 w-full -z-10' 
+        style={{background: `linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%)`}}></div>
+
+      <div className="  flex justify-center items-center flex-col">
+        <motion.div 
+          animate={{ opacity: 1, y:0 }}
+          initial={{ opacity: 0, y:-50 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration :0.5 }}
+          className="text-white h-[70vh] flex justify-center items-center">
+          <img src={process.env.PUBLIC_URL+'images/logo01.png'} alt="" />
+        </motion.div>
+        <div className="text-white">
+          <div className="text-zinc-300">請輸入陽間的手機號碼登入世界</div>
+          <form >
+            <div className="my-3">
+              <input
+                type="email"
+                placeholder="手機號碼"
+                className="form-control loginInput text-sm w-full tex-center pb-1"
+              />
+            </div>
+            <button
+              className="py-3 px-4  text-white w-full rounded-full text-sm tracking-wider my-10 hover:brightness-110"
+              style={{background: `linear-gradient(90deg, rgba(38,94,234,1) 0%, rgba(107,237,251,1) 100%)`}}
+              
+            >
+              開始遊戲
+            </button>
+          </form>
+        </div>
+        
+        
+      </div>
+
     </div>
   );
 }
