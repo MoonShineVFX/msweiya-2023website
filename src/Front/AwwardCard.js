3da37f53be5c83ee21e810b3db26ed491b2b18e7
wood.wu <kilokingw@gmail.com>
2023-01-18 13:33:48 +0800

    Update AwwardCard.js

--- src/Front/AwwardCard.js
+++ src/Front/AwwardCard.js
@@ -15,17 +15,22 @@ function AwwardCard({data,index,groupData}) {
         </div>
         <div 
           id="bottom"
-          className={'bg-white absolute p-12 h-full aspect-[10/8] rounded-md  bg-cover bg-no-repeat bg-center z-0 left-[50%]   transition-all '+ (active ? ' -translate-x-[20%] ' : '  -translate-x-[50%]')}
+          className={'bg--100 absolute p-8 h-full aspect-[10/8] rounded-md  bg-cover bg-no-repeat bg-center z-0 left-[50%]   transition-all '+ (active ? ' -translate-x-[15%] ' : '  -translate-x-[49%]  translate-y-[2%]')}
+          style={{background:`radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%),
+          radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)`}}
         >
-          <div className='border-2 h-full rounded-md border-zinc-400 text-black flex justify-center items-center gap-3 flex-wrap px-4 overflow-hidden overflow-y-auto'>
-            {
-              groupData&&
-              groupData[index].map((item,index)=>{
-                return(
-                  <div key={'user'+index} className="text-3xl break-keep">{item.name}</div>
-                )
-              })
-            }
+          <div className='border-2 h-full rounded-md border-amber-300 text-black flex justify-center items-center gap-3 flex-wrap px-4 overflow-hidden overflow-y-auto'>
+            <div className='  mx-auto flex flex-wrap'>
+              {
+                groupData&&
+                groupData[index].map((item,index)=>{
+                  return(
+                    <div key={'user'+index} className="text-3xl break-keep font-bold text-white  drop-shadow-xl p-1">{item.name}</div>
+                  )
+                })
+              }
+            </div>
+
 
           </div>
         </div>
